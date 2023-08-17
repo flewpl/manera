17ff455b2a9012e9e68cc337554a3ddb74f69cec
flewpl <74003907+flewpl@users.noreply.github.com>
2023-08-17 11:41:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import pixellib 
-import cv2 
+import cv2
+import torch
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
+import keras
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s') # change to yolov5x
+
+video_path = "video.mp4"
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    results = model(frame)
+
+
+    cv2.imshow("TOLO",np.squeeze(results.render()))
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
